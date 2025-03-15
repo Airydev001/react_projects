30e46ceda31be3e03155eb6bc7cc2ec8728a7bf4
airydev001 <airydev585@gmail.com>
2025-03-15 09:19:50 +0100

    Journal App in react

--- development_react/src/App.jsx
+++ development_react/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-
+import Header from './components/header';
+import Entry from './components/Entry';
+import Data from './data'
 function App() {
   return (
     <div>
-      <Navbar />
-      <Main />
+     <Header/>
+     {
+      Data.map((data)=>(
+        <Entry key = {data.id} img={data.img}
+        title={data.title}
+        country = {data.country}
+        googleMapsLink = {data.googleMapsLink}
+        dates={data.dates}
+        text = {data.text}
+        />
+      ))
+     }
+    
+     
     </div>
   );
 }
