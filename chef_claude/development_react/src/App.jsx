e1fd4b6f3a941f4232324340c481722fc104f57a
airydev001 <airydev585@gmail.com>
2025-03-18 10:44:11 +0100

    how to use set state in react first practice

--- chef_claude/development_react/src/App.jsx
+++ chef_claude/development_react/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Fav from "./components/fav"
 
 export default function App() {
 return (
   <>
-   <Header/>
-   <Main/></>
+   {/* <Header/>
+   <Main/> */}
+   <Fav/>
+   </>
 
 )  
 }
---
+++ chef_claude/development_react/src/components/fav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Fav(){
+    const [myFavoriteThings, setMyFavoriteThings]= React.useState([]) 
+   
+    const allFavoriteThings = [
+        "Mercedes","Toyota","Nissan","Roll Royce", "Mistubishi"
+    ];
+
+    const thingsElements = myFavoriteThings.map(thing => <p key ={thing}>{thing}</p>)
+
+    function addFavoriteThings(){
+        setMyFavoriteThings(
+           prevFavThings => [...prevFavThings,"Test"] 
+        )
+    }
+
+    return(
+        <main>
+            <button onClick = {addFavoriteThings}>Add item</button>
+        <section aria-live = "polite">
+            {thingsElements}
+        </section>
+        
+        </main>
+    )
+
+}
