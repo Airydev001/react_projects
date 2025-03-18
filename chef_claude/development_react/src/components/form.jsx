5a54f0edde29909ea865d34ee5f05fb5f8aa4763
airydev001 <airydev585@gmail.com>
2025-03-18 14:49:27 +0100

    form data callng the sign up function

--- chef_claude/development_react/src/components/form.jsx
+++ chef_claude/development_react/src/components/form.jsx
@@ -4,20 +4,19 @@ import React from "react";
 
 export default function Form(){
 
-    function handleSubmit(event){
-        event.preventDefault()
-        const formEl = event.currentTarget
-        const formData = new FormData(formEl);
-        const email = formData.get("email")
+    function signUp(formData){
+        const email = formData.get("email");
         const password = formData.get("password")
+        console.log({
+            email: email, password:password
 
-        console.log([email,password])
-        formEl.reset()
+        })
+
     }
     return (
         <section className="section_container">
           <h1>Signup form</h1>
-          <form action="" onSubmit={handleSubmit} method="post" className="form_container">
+          <form action={signUp}  className="form_container">
             <label htmlFor="email">Email:   </label>
 
                 <input id = "email" type="email" 
