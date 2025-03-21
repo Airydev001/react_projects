47cf13601f8d6532fc7d38b7f5319b7f1f014ada
airydev001 <airydev585@gmail.com>
2025-03-21 14:13:47 +0100

    happy use of use effect

---
+++ meme-generator/src/components/Main.jsx
@@ -0,0 +1,57 @@
+import {useState, useEffect} from "react";
+
+export default function Main() {
+
+
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => console.log(data.data.memes))
+    })
+
+    function handleChange(event){
+        const {value,name} = event.currentTarget;
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
