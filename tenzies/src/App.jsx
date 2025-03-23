369a3655a75375722ee6b0f0e639e53f5b2bdaa7
airydev001 <airydev585@gmail.com>
2025-03-23 11:53:20 +0100

    array method from the AI chatgpt

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {useState} from "react"
+import Die from "./Die"
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+
+export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice()); 
+function generateAllNewDice(){
+ 
+  const randomRolledNumber = Array.from({length:10},()=>Math.floor(Math.random() *6 )+ 1);
+
+  return randomRolledNumber;
+
+
+
 }
 
-export default App
+function rollDice(){
+  setDice(generateAllNewDice())
+}
+
+const diceElements = dice.map(num => <Die value={num} />)
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+               
+            </div>
+            <button className="roll-dice" onClick= {rollDice}>Roll Dice</button>
+        </main>
+    )
+}
